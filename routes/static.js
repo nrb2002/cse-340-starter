af487443adb768ac1520edc8899e89464da5b225
nrb2002 <tshibasubaron@yahoo.fr>
2025-09-14 09:55:25 +0100

    New update
    All bugs fixed.

--- routes/static.js
+++ routes/static.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express'); //Import express package
+const router = express.Router(); //Use express router
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
+
+//Express router is to "use" the "express.static" function, meaning this is where static resources will be found, with the public folder.
 router.use(express.static("public"));
+//Any route that contains /css is to refer to the public/css folder, which is found at the root level of the project.
 router.use("/css", express.static(__dirname + "public/css"));
+//any route that contains /js is to refer to the public/js folder, which is found at the root level of the project.
 router.use("/js", express.static(__dirname + "public/js"));
+//any route that contains /images is to refer to the public/images folder, which is found at the root level of the project.
 router.use("/images", express.static(__dirname + "public/images"));
 
+//Export module to be used in other areas of the application
 module.exports = router;
 
 
--- server.js
+++ server.js
@@ -5,15 +5,33 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express") //Import express package
+const expressLayouts = require("express-ejs-layouts") //Import ejs
+const env = require("dotenv").config() //Import environment variables
+const app = express() //Create express application
+const static = require("./routes/static") //Import static routes
+ 
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
+/**The resource which has been exported in the static file is now to be used by the app. 
+ * This single line of code now allows the app to know where the public folder is located and that it and all of its subfolders will be used for static files.
+*/
 app.use(static)
+
+//Index route
+//This is the base route fetched by a function that takes the request and response objects as parameters.
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"}) //Express function that will retrieve the specified view - "index" - to be sent back to the browser.
+})
+
 
 /* ***********************
  * Local Server Information
